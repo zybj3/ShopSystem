72e28cba1d727352b752aa4f0d65b96337dc9f56
zybj3 <137526723@qq.com>
2019-10-27 16:32:01 -0700

    create login controller and register controller

---
+++ src/main/java/com/osu/shopsystem/web/user/LoginController.java
@@ -0,0 +1,12 @@
+package com.osu.shopsystem.web.user;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class LoginController {
+    @RequestMapping("/login")
+    private String loginPage() {
+        return "user/login";
+    }
+}
