ef765111cd69a176821b515721dd9938422a6711
MadForCoding <chenw5@oregonstate.edu>
2019-10-27 18:03:02 -0700

    Login in Test

---
+++ src/test/java/com/osu/shopsystem/dao/PersonInfoDaoTest.java
@@ -0,0 +1,47 @@
+package com.osu.shopsystem.dao;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.osu.shopsystem.BaseTest;
+import com.osu.shopsystem.entity.PersonInfo;
+
+public class PersonInfoDaoTest extends BaseTest {
+
+    @Autowired
+    PersonInfoDao personInfoDao;
+
+    @Test
+    public void testAddPersonInfo(){
+        PersonInfo personInfo = new PersonInfo();
+        personInfo.setName("test add person");
+        personInfo.setCreateTime(new Date());
+        personInfo.setLastEditTime(new Date());
+        personInfo.setPassword("PASS");
+        personInfo.setEmail("test email");
+        personInfo.setGender("male");
+        personInfo.setUserType(1);
+        int effectiveNum = personInfoDao.addPersonInfo(personInfo);
+        System.out.println(effectiveNum);
+    }
+
+    @Test
+    public void testQueryPersonInfoByUserName(){
+        String userName = "G01 person";
+        int count = personInfoDao.queryPersonInfoByUserName(userName);
+        System.out.println(count);
+
+    }
+
+    @Test
+    public void testQueryIfUserExist(){
+        String userName = "G01 person";
+        String password = "12308811";
+        PersonInfo personInfo= personInfoDao.queryIfUserExist(userName, password);
+        System.out.println(personInfo);
+    }
+
+
+}
