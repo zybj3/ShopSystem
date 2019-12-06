e18d5b0aec3a947c62c4073877247b3a469e53a2
ChenQ94 <32177304+ChenQ94@users.noreply.github.com>
2019-12-05 19:37:48 -0800

    productdetail.js file

---
+++ src/main/webapp/resources/js/frontend/productdetail.js
@@ -0,0 +1,42 @@
+$(function() {
+	var productId = getQueryString('productId');
+	var productUrl = '/dianping/frontend/listproductdetailpageinfo?productId='
+		+ productId;
+	$.getJSON(productUrl, function(data) {
+		if (data.success) {
+			var product = data.product;
+			$('#product-img').attr('src', product.imgAddr);
+			$('#product-time').text(
+				new Date(product.lastEditTime));
+			// if (product.point != undefined) {
+			// 	$('#product-point').text('购买可得' + product.point + '积分');
+			// }
+			$('#product-name').text(product.productName);
+			$('#product-desc').text(product.productDesc);
+			if (product.normalPrice != undefined
+				&& product.promotionPrice != undefined) {
+				$('#price').show();
+				$('#normalPrice').html(
+					'<del>' + '￥' + product.normalPrice + '</del>');
+				$('#promotionPrice').text('$' + product.promotionPrice);
+			} else if (product.normalPrice != undefined
+				&& product.promotionPrice == undefined) {
+				$('#price').show();
+				$('#promotionPrice').text('$' + product.normalPrice);
+			} else if (product.normalPrice == undefined
+				&& product.promotionPrice != undefined) {
+				$('#promotionPrice').text('￥' + product.promotionPrice);
+			}
+			var imgListHtml = '';
+			product.productImgList.map(function(item, index) {
+				imgListHtml += '<div> <img src="' + item.imgAddr
+					+ '" width="100%" /></div>';
+			});
+			$('#imgList').html(imgListHtml);
+		}
+	});
+	$('#me').click(function() {
+		$.openPanel('#panel-right-demo');
+	});
+	$.init();
+});
