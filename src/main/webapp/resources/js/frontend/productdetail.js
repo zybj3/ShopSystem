2f3d109cd69523cc60cbdd0f9f0dfd2204a8ce25
ChenQ94 <32177304+ChenQ94@users.noreply.github.com>
2019-12-05 20:13:00 -0800

    productdetail.js file fix the dollar icon

--- src/main/webapp/resources/js/frontend/productdetail.js
+++ src/main/webapp/resources/js/frontend/productdetail.js
@@ -8,16 +8,13 @@ $(function() {
 			$('#product-img').attr('src', product.imgAddr);
 			$('#product-time').text(
 				new Date(product.lastEditTime));
-			// if (product.point != undefined) {
-			// 	$('#product-point').text('购买可得' + product.point + '积分');
-			// }
 			$('#product-name').text(product.productName);
 			$('#product-desc').text(product.productDesc);
 			if (product.normalPrice != undefined
 				&& product.promotionPrice != undefined) {
 				$('#price').show();
 				$('#normalPrice').html(
-					'<del>' + '￥' + product.normalPrice + '</del>');
+					'<del>' + '$' + product.normalPrice + '</del>');
 				$('#promotionPrice').text('$' + product.promotionPrice);
 			} else if (product.normalPrice != undefined
 				&& product.promotionPrice == undefined) {
@@ -25,11 +22,11 @@ $(function() {
 				$('#promotionPrice').text('$' + product.normalPrice);
 			} else if (product.normalPrice == undefined
 				&& product.promotionPrice != undefined) {
-				$('#promotionPrice').text('￥' + product.promotionPrice);
+				$('#promotionPrice').text('$' + product.promotionPrice);
 			}
 			var imgListHtml = '';
 			product.productImgList.map(function(item, index) {
-				imgListHtml += '<div> <img src="' + item.imgAddr
+				imgListHtml += '<div> < img src="' + item.imgAddr
 					+ '" width="100%" /></div>';
 			});
 			$('#imgList').html(imgListHtml);
